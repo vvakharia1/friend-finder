577ae06e071bbadfaa2a694c7bb6deca37824d8a
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-25 11:09:09 -0400

    created friends array in data

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friends = [
+  {
+    name: "Daenerys Targaryen",
+    photo:
+      "https://timedotcom.files.wordpress.com/2019/05/game-of-thrones-s8-e6-daenerys-targaryen-iron-throne.jpeg",
+    score: [1, 2, 4, 5, 3, 4, 2, 3, 1, 2]
+  },
+  {
+    name: "Jon Snow",
+    photo:
+      "https://vignette.wikia.nocookie.net/gameofthrones/images/d/d0/JonSnow8x06.PNG/revision/latest?cb=20190714094440",
+    score: [3, 4, 5, 3, 2, 1, 3, 4, 5, 3]
+  },
+  {
+    name: "Arya Stark",
+    photo:
+      "https://vignette.wikia.nocookie.net/gameofthrones/images/b/be/AryaShipIronThrone.PNG/revision/latest?cb=20190520174300",
+    score: [3, 5, 5, 4, 4, 3, 2, 3, 4, 1]
+  },
+  {
+    name: "Cersei Lannister",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/2/22/Cersei_Lannister_in_Black_Dress_in_Season_5.jpg/220px-Cersei_Lannister_in_Black_Dress_in_Season_5.jpg",
+    score: [4, 5, 4, 3, 2, 3, 4, 5, 3, 2]
+  },
+  {
+    name: "Tyrion Lannister",
+    photo:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/44ccdd999a4b20cdaef4696b8bf1a2c502db2b66f6909ae2a8a96a01a698370688e9b73dad75f0d33ea170c58a251155-1-1558322295.jpg?crop=0.668xw:1.00xh;0.0918xw,0&resize=480:*",
+    score: [5, 4, 2, 3, 4, 5, 3, 2, 4, 1]
+  }
+];
